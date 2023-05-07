3cff815bb9d35b41251e3f5d263b2f2cb69f94b2
Valeriia Isaienko <93624578+Magic1vy@users.noreply.github.com>
2023-05-06 22:13:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const canvas = document.querySelector("#draw");
 const ctx = canvas.getContext("2d");
+const colorPicker = document.querySelector("#color-picker");
+const brushSize = document.querySelector("#brush-size");
+const useCustomColor = document.querySelector("#use-custom-color");
+const useCustomSize = document.querySelector("#use-custom-size");
+const bgColorPicker = document.querySelector("#bg-color-picker");
 
 canvas.width = window.innerWidth; 
 canvas.height = window.innerHeight;
 
-ctx.strokeStyle = "#BADA55";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
 ctx.lineWidth = 1;
@@ -13,30 +17,33 @@ let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
-let direction = true 
+let direction = true;
 
-function draw(x, y){
+function draw(x, y) {
     if (!isDrawing) return;
-    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
+    if (useCustomColor.checked) {
+        ctx.strokeStyle = colorPicker.value;
+    } else {
+        ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+        hue = (hue + 1) % 360;
+    }
+
+    if (useCustomSize.checked) {
+        ctx.lineWidth = brushSize.value;
+    } else {
+        ctx.lineWidth = direction ? ctx.lineWidth + 1 : ctx.lineWidth - 1;
+        if (ctx.lineWidth >= 50 || ctx.lineWidth <= 1) {
+            direction = !direction;
+        }
+    }
+
     ctx.beginPath();
     ctx.moveTo(lastX, lastY);
     ctx.lineTo(x, y);
-    ctx.stroke(); 
-    hue++;
+    ctx.stroke();
 
-    if (hue > 360){
-            hue = 0;
-    }
-    if (ctx.lineWidth >= 50 || ctx.lineWidth <= 1){
-            direction = !direction; 
-    }
-    if (direction) {
-        ctx.lineWidth++; 
-    } else {
-        ctx.lineWidth--; 
-    }
-
-    [lastX, lastY] = [x, y]
+    [lastX, lastY] = [x, y];
 }
 
 function handleMouseDown(e) {
@@ -63,6 +70,10 @@ function handleMouseUp() {
     isDrawing = false;
 }
 
+function setBackgroundColor() {
+    canvas.style.backgroundColor = bgColorPicker.value;
+}
+
 canvas.addEventListener("mousedown", handleMouseDown);
 canvas.addEventListener("touchstart", handleTouchStart, { passive: false });
 canvas.addEventListener("mousemove", handleMouseMove);
@@ -70,3 +81,5 @@ canvas.addEventListener("touchmove", handleTouchMove, { passive: false });
 canvas.addEventListener("mouseup", handleMouseUp);
 canvas.addEventListener("mouseout", handleMouseUp);
 canvas.addEventListener("touchend", handleMouseUp, { passive: false });
+
+bgColorPicker.addEventListener("change", setBackgroundColor);
